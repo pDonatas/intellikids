6e8a24d2f54b2992ba3db3afa2119f65ab819a09
donatas <petrikauskas.donatas@gmail.com>
2023-11-28 21:30:20 +0200

    storage permissions fix

--- init.sh
+++ init.sh
@@ -9,3 +9,7 @@ docker compose exec app composer install
 docker compose exec app php artisan key:generate
 docker compose exec app php artisan migrate
 docker compose exec app php artisan db:seed
+docker compose exec app chmod -R 777 storage
+
+docker compose stop
+docker compose up -d
